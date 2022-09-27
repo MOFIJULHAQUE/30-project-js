c32487d1782d5f8cdcdd62ffa918732a08e2a163
MofijulHaque <mofijulhoque995@gmail.com>
2022-09-27 16:18:12 +0530

    Count-down-timer

--- Weather_app/app.js
+++ Weather_app/app.js
@@ -16,7 +16,7 @@ const getWeather = async (city) => {
 const showWeather = (data) => {
     if (data.cod == "404") {
         weather.innerHTML = `<h2> City Not Found <h2>`
-        return;
+        return;\o3DukCgvUg0
     }
     weather.innerHTML = `
         <div>
